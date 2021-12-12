c5d8cdb7dddcee687ce9bb3bbc1934d945e938be
yrastop <73952941+yrastop@users.noreply.github.com>
2021-12-12 16:20:30 -0500

    Debugged and ready for submission

--- RSA_main.py
+++ RSA_main.py
@@ -2,45 +2,69 @@ import math
 
 def RSA(p:int, q:int, msg:str):
 
+    # Calculate n = pq
     n = p*q 
+    # Calculate the totient function, phi(n) = (q-1)(p-1) 
     phi = (q-1)*(p-1)
 
+    #Calculate an e value that is coprime to phi(n)
+    # e = euc_gcd(e, phi) -> this is your public key 
     e = calculate_e(phi)
-    print("Your public key is: ", e)
+    print("Your public key is: (" + str(e) + ", " + str(n) + ")")
+    #Calculate d where e*d = 1mod(n), makes up the private key
     d = calculate_d(e, phi)
-    print("Your private key is: ", d)
+    print("Your private key is: (" + str(d) + ", " + str(n) + ")")
 
     cypher_txt = ''
     
-    for char in msg:
+    
+    for ch in msg:
+        #Converts message into cypher text 
+        
+        #Replaces all characters with ASCii symbols, encrypts characters 
+        char = ord(ch); 
+        print(str(ch) + ": " + str(char))
 
-        char = ord(char); 
+        # C = (P^e) mod n
         cypher_txt += chr((char ** e) % n)
     
     plain_txt = ''
 
-    for char in cypher_txt:
+    for ch in cypher_txt:
+        #Decrypts encrypted text 
 
-        char = ord(char); 
+        #Convert to ASCii 
+        char = ord(ch); 
+        print(str(ch) + ": " + str(char))
 
+        #Convert characters in order to reconstruct message
+        # P = (C^d) mod n
         plain_txt += chr((char ** d) % n)
+
 
     return(cypher_txt, plain_txt)
 
 
 def calculate_e(phi: int): 
+    # Calculate the public key based on phi(n)
+    # e must meet the conditions 1 < e < phi(n) and coprime to phi(n)
 
     e = 2
     while e < phi: 
+        # If e is coprime to phi(n), gcd will be 1
         if euc_gcd(e, phi) == 1:
             return e
         e+=1
 
 def calculate_d(e: int, phi: int): 
+    # Calculate private key based on e and phi(n)
+    # d = e^-1 mod phi(n)
     d = pow(e, -1, phi)
     return d
-    
+
 def euc_gcd(x: int, y: int):
+    # Use the Euclidean algorithm to find the greatest 
+    # common divisor of x and y
     
     small,large = (x,y) if x<y else (y,x)
 
